0a4669138f5d1664c614a2575857d91adc698882
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-04-01 21:51:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
 end
 
 def full?(board)
-  board.all? {|taken| board[taken] == "X" && board(taken) == "0"}
+  board.all? {|taken| board[taken] == "X" && board[taken] == "0"}
     return true
   end
 end
